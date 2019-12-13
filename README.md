e9780fe359be23ce6ff66055b75c18bf61463d4b
luke-ols0 <luke.olson@outlook.com>
2019-12-12 23:46:07 -0600

    push final build

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # githubUserInfoToPDF
+# Luke Olson
+
+Takes user's GitHub username and favorite color to create a PDF document which shows
+    the user's name,
+    occupation, 
+    link of location,
+    link of GitHub URL,
+    link of blog URL,
+    number of public repositories,
+    number of followers,
+    number of GitHub Stars,
+    and number of following.
+
+Code uses Axios and GitHub api to pull results into a class
+
+Instructions:
+    1. Download or clone repository
+    2. Enter repository location in terminal
+    3. Run 'node index.js'
+    4. Enter your GitHub username
+    5. Enter your favorite color (which will be the background)
+    6. Open the generated PDF file which will be named 'pdf-output.pdf'
--- index.js
+++ index.js
@@ -14,11 +14,16 @@ class Program {
     promptUser() {
         return inquirer.prompt([
             {
-                message: 'What is your user name',
+                message: 'What is your GitHub user name?:',
                 name: 'githubUserName'
+            },
+            {
+                message: 'What is your favorite color?:',
+                name: 'favColor'
             }
-        ]).then(({ githubUserName }) => {
+        ]).then(({ githubUserName, favColor }) => {
             this.githubUserName = githubUserName;
+            this.favColor = favColor;
             this.makeApiRequest();
         })
     }
@@ -41,7 +46,8 @@ class Program {
                             bio,
                             public_repos,
                             followers,
-                            following
+                            following,
+                            company
                         }
                     },
                     {
@@ -55,6 +61,7 @@ class Program {
                 this.avatar_url = avatar_url;
                 this.location = location;
                 this.name = name;
+                this.company = company;
                 this.blog = blog;
                 this.bio = bio;
                 this.public_repos = public_repos;
@@ -75,32 +82,150 @@ class Program {
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
             <title>GitHubPortfolio</title>
         </head>
         
         <style>
             body {
-                background-color: powderblue;
+                background-color: ${this.favColor};
+                color: white;
+                text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
             }
         
             div.intro {
-                background-color: grey;
+                background-color: orange;
                 text-align: center;
                 margin-top: 50px;
-                margin-left: 200px;
-                margin-right: 200px;
+                margin-left: 100px;
+                margin-right: 100px;
+                margin-bottom: 50px;
+                border-radius: 10px;
+                font-family: Roboto;
+                font-weight: bold;
             }
+
+            div.links{
+                margin-left: 40px;
+                text-align: left;
+                padding-bottom: 20px;
+                font-size: 20px;
+            }
+
+            div.bio{
+                text-align: center;
+                margin-bottom: 50px;
+            }
+
+            div.repo{
+                background-color: orange;
+                width: 300px;
+                height: 100px;
+                text-align: center;
+                padding-top: 5px;
+                padding-bottom: 50px;
+                margin-left: 60px;
+                border-radius: 10px;
+                display: inline-block;
+            }
+
+            div.followers{
+                background-color: orange;
+                width: 300px;
+                height: 100px;
+                text-align: center;
+                padding-top: 5px;
+                padding-bottom: 50px;
+                border-radius: 10px;
+                display: inline-block;
+                margin-left: 40px;
+            }
+
+            div.stars{
+                background-color: orange;
+                width: 300px;
+                height: 100px;
+                text-align: center;
+                padding-top: 5px;
+                padding-bottom: 50px;
+                margin-left: 60px;
+                margin-top: 40px;
+                border-radius: 10px;
+                display: inline-block;
+            }
+
+            div.following{
+                background-color: orange;
+                width: 300px;
+                height: 100px;
+                text-align: center;
+                padding-top: 5px;
+                margin-top: 40px;
+                padding-bottom: 50px;
+                border-radius: 10px;
+                display: inline-block;
+                margin-left: 40px;
+            }
+
+            div.githubinfo2{
+                margin-bottom: 50px;
+            }
+
         </style>
         
         <body>
         
             <body>
                 <div class="intro">
-                    <img src="${this.avatar_url}" alt="GitHub Avatar" height="200" width="200" style="
-                    padding-top: 50px;">
-                    <h1 style="font-size:30px;">Hi!</h1>
-                    <h2>My Name is ${this.name}!</h2>
+
+                    <img src="${this.avatar_url}" alt="GitHub Avatar" height="200" width="200"
+                    style="padding-top: 50px;">
+                    <h1 style="font-size:50px;">Hi!</h1>
+                    <h2 style="font-size:40px;">My Name is ${this.name}!</h2>
+                    <h2 style="font-size:30px;">Currently @ ${this.company}</h2>
+        
+                    <div class="links">
+                        <a href="https://www.google.com/maps/place/${this.location}" style="color: white; font-weight: bold;">${this.location}</a>
+                        <a href="https://github.com/${this.githubUserName}" style="color: white; font-weight: bold; padding-left: 70px;">GitHub</a>
+                        <a href="${this.blog}" style="color: white; font-weight: bold; padding-left: 130px;">Blog</a>
+                    </div>
+
                 </div>
+
+                <div class="bio">
+
+                    <h1 style="font-size:30px;">${this.bio}</h1>
+
+                </div>
+
+                <div class="githubinfo1">
+
+                    <div class="repo">
+                    <h2 style="font-size:30px;">Public Repositories</h2>
+                    <h2 style="font-size:30px;">${this.public_repos}</h2>
+                    </div>
+                
+                    <div class="followers">
+                    <h2 style="font-size:30px;">Followers</h2>
+                    <h2 style="font-size:30px;">${this.followers}</h2>
+                    </div>
+
+                </div>
+
+                <div class="githubinfo2">
+
+                    <div class="stars">
+                    <h2 style="font-size:30px;">GitHub Stars</h2>
+                    <h2 style="font-size:30px;">${this.stars}</h2>
+                    </div>
+            
+                    <div class="following">
+                    <h2 style="font-size:30px;">Following</h2>
+                    <h2 style="font-size:30px;">${this.following}</h2>
+                    </div>
+
+            </div>
+
             </body>
         
         </html>
@@ -110,7 +235,7 @@ class Program {
     }
 
     createPdf() {
-        pdf.create(this.html).toFile('./class-test.pdf', function (err, res) {
+        pdf.create(this.html).toFile('./pdf-output.pdf', function (err, res) {
             if (err) return console.log(err);
             console.log(res);
         });
